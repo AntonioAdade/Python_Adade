2ba79e736faf6032f3ae09f29a795e2197a245c7
AACB <antonio.adade@hotmail.com>
2024-09-18 14:52:27 -0300

    subindo arquivos python

---
+++ print_input.py
@@ -0,0 +1,6 @@
+nome = input("informe seu nome ")
+idade = input("informe sua idade ")
+print (nome, idade, end= "\n")
+print (nome)
+print (idade)
+
---
+++ variaveis_constantes.py
@@ -0,0 +1,12 @@
+nome = "Adade"
+idade = 35
+
+nome ="Antonio"
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+ESTADOS = ("SP", "RJ", "SC", "MG")
+
+print (ESTADOS)
