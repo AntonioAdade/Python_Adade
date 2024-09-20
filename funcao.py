e5cf94669ed4b0a12b630b003046a457fff621dc
AACB <antonio.adade@hotmail.com>
2024-09-20 00:21:58 -0300

    funções

---
+++ funcao.py
@@ -0,0 +1,141 @@
+#funçoes
+
+def exibir_mensagem():
+    print("Olá mundo!")
+
+
+def exibir_mensagem_2(nome):
+    print(f"Seja bem vindo {nome}!")
+
+
+def exibir_mensagem_3(nome="Anônimo"):
+    print(f"Seja bem vindo {nome}!")
+
+
+exibir_mensagem()
+exibir_mensagem_2(nome="Guilherme")
+exibir_mensagem_3()
+exibir_mensagem_3(nome="Chappie")
+print("##############")
+
+##########################################
+# funçao retorno
+
+def calcular_total(numeros):
+    return sum(numeros)
+   
+def retorna_antecessor_e_sucessor(numero):
+    antecessor = numero - 1
+    sucessor = numero + 1
+
+    return antecessor, sucessor
+
+print(calcular_total([10, 20, 34]))
+print(retorna_antecessor_e_sucessor(10))
+
+
+###############################################
+#argumentos nomeados
+
+def salvar_carro(marca, modelo, ano, placa):
+    # #salva carro no banco de dados...
+    print(f"Carro inserido com sucesso! {marca}/{modelo}/{ano}/{placa}")
+
+salvar_carro("Fiat", "Palio", 1999, "ABC-1234")
+salvar_carro(marca="Fiat", modelo="Palio", ano=1999, placa="ABC-1234")
+salvar_carro(**{"marca": "Fiat", "modelo": "Palio", "ano": 1999, "placa": "ABC-1234"}) #asterisco duplo significa biblioteca
+print()
+print("##############")
+  
+###############################################################
+#*arg e **kwarg
+
+def exibir_poema(data_extenso, *args, **kwargs):
+    texto = "\n".join(args)
+    meta_dados = "\n".join([f"{chave.title()}: {valor}" for chave, valor in kwargs.items()])
+    mensagem = f"{data_extenso}\n\n{texto}\n\n{meta_dados}"
+    print(mensagem)
+
+
+exibir_poema(
+    "quinta-feira, 19 de setembro de 2024", #poemo do python...
+    "Zen of Python",
+    "Beautiful is better than ugly.",
+    "Explicit is better than implicit.",
+    "Simple is better than complex.",
+    "Complex is better than complicated.",
+    "Flat is better than nested.",
+    "Sparse is better than dense.",
+    "Readability counts.",
+    "Special cases aren't special enough to break the rules.",
+    "Although practicality beats purity.",
+    "Errors should never pass silently.",
+    "Unless explicitly silenced.",
+    "In the face of ambiguity, refuse the temptation to guess.",
+    "There should be one-- and preferably only one --obvious way to do it.",
+    "Although that way may not be obvious at first unless you're Dutch.",
+    "Now is better than never.",
+    "Although never is often better than *right* now.",
+    "If the implementation is hard to explain, it's a bad idea.",
+    "If the implementation is easy to explain, it may be a good idea.",
+    "Namespaces are one honking great idea -- let's do more of those!",
+    autor="Tim Peters",
+    ano=1999,
+)
+
+print()
+print("####################################")
+
+###################################################
+#parametros por nome e posição
+def criar_carro(modelo, ano, placa, /, marca, motor, combustivel):
+    print(modelo, ano, placa, marca, motor, combustivel)
+
+
+criar_carro("Palio", 1999, "ABC-1234", marca="Fiat", motor="1.0", combustivel="Gasolina")
+#criar_carro(modelo="Palio", ano=1999, placa="ABC-1234", marca="Fiat", motor="1.0", combustivel="Gasolina")  # inválido
+
+print()
+print("########################################")
+
+####################################################################
+#Objeto de primeira classe
+
+def somar(a, b):
+    return a + b
+
+def subtrair(a, b):
+    return a + b
+
+def exibir_resultado(a, b, funcao):
+    resultado = funcao(a, b)
+    print(f"O resultado da operação é = {resultado}")
+
+
+exibir_resultado(10, 10, somar)
+exibir_resultado(10, 10, subtrair)
+
+print()
+print("#######################################################")
+
+###################################################################
+#classe local e global
+
+salario = 2000
+
+
+def salario_bonus(bonus):
+    global salario #as variaveis declaradas fora da função não são reconhecidas pelo corpo da função
+    salario += bonus #a palavra global força utilizar a variavel fora da função dentro da função
+    return salario
+
+salario_com_bonus = salario_bonus(500)
+print(f"o salario com bonua é: ", salario_com_bonus)
+
+
+
+
+
+
+
+
